7828cb78f75cdc26c2ac64027c43aab500712181
Denzel Pressey <pressey90@gmail.com>
2013-12-19 02:59:52 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JasonWebsite::Application.config.secret_key_base = '51ba94e63ecf6400f3681ec2c35c0603471f3df685fc68aedd158fe43b048f277840f4384954f4fcdc290cf83789036f0ddbdda5ad21bd791b42728ce3099e99'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JasonWebsite::Application.config.session_store :cookie_store, key: '_jason_website_session'
